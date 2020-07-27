ec3f21a3e0791799a06eb8c026ba396ef8595b7e
Cesar Martinez <cecam92@hotmail.com>
2020-07-28 01:55:05 +1000

    Updates

---
+++ precache-manifest.12be89323dbabd41edd1bb2c4b81e8b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6ba1ad14cf721735724c2661dd9438",
+    "url": "/Galeria/index.html"
+  },
+  {
+    "revision": "1a72d89d116fd681a86f",
+    "url": "/Galeria/static/css/main.a07addd7.chunk.css"
+  },
+  {
+    "revision": "476e6579300aea8c9e30",
+    "url": "/Galeria/static/js/2.9b814eab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Galeria/static/js/2.9b814eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a72d89d116fd681a86f",
+    "url": "/Galeria/static/js/main.f17d0816.chunk.js"
+  },
+  {
+    "revision": "13f6147c50e4bddef619",
+    "url": "/Galeria/static/js/runtime-main.adb63574.js"
+  },
+  {
+    "revision": "1e1d0cf31b39028c9ad58653e5240d69",
+    "url": "/Galeria/static/media/kami.1e1d0cf3.png"
+  }
+]);
